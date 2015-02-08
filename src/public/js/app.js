5cf6d5d71824105538c65d3a4aa5f2f059543c4b
David Dias <daviddias.p@gmail.com>
2015-02-08 22:20:01 +0000

    aaaa

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -307,10 +307,44 @@ window.app = {
     init: function () {
         console.log('starting');
 
-        var id1 = new Id();
+        var R = 150 
+        var peers = [];
+       
+        // create peers
+        for (i=0;i<10;i++) {
+            peers.push(new Id());
+        }
+        
+        // add their coordinates 
+        peers.forEach(function (peer) {
+            peer.coordinates = cartesianCoordinates(peer.toDec(), R);
+        });
 
-        console.log(cartesianCoordinates(id1.toDec(), 10));
+        console.log('PEERS', peers);
+        
+        var vis = d3.select('#dht-ring')
+                    .append('svg');
 
+        vis.attr("width", 600)
+            .attr("height", 600);
+
+        var peer = vis.selectAll("circle.peers")
+            .data(peers)
+            .enter()
+            .append("svg:circle")
+            .attr("cx", function(peer) { return peer.coordinates.x + 1.2 * R + 50; })
+            .attr("cy", function(peer) { return peer.coordinates.y + 1.2 * R; })
+            .attr("r", "5px")
+            .attr("fill", "black")
+
+        peer.append("svg:text")
+            .attr("dx", 12)
+            .attr("dy", ".35em")
+            .text(function(d) { return d.toHex(); });
+
+            //.append("text").text(function(peer, i) { return peer.toHex(); });
+
+        console.log('finished');
 
 
     }
